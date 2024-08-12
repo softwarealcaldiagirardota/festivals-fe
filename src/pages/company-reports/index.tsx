094be683a6255ea0680ee6ad6727f752df652b5e
Arlex296 <106871942+Arlex296@users.noreply.github.com>
2024-08-12 16:02:50 -0500

    organize dinamic title (#4)
    Co-authored-by: Arlex0296 <edwinbustamante2010@gmail.com>

--- src/pages/company-reports/index.tsx
+++ src/pages/company-reports/index.tsx
@@ -5,14 +5,23 @@ import logoAction1 from "../../assets/splash-2.png";
 import logoAction2 from "../../assets/splash-3.png";
 import { StyledContainerActions } from "./styles";
 import { useNavigate } from "react-router-dom";
+import { useAuth0 } from "@auth0/auth0-react";
+
+
 
 const CompanyReports = () => {
   const { setTitle, isMobile } = useHeader();
   const navigate = useNavigate();
-  useEffect(() => {
-    setTitle("Soy participante");
-  }, []);
+  const { user } = useAuth0();
 
+  useEffect(() => {
+    if (user && user.nickname) {
+      const capital =  user.nickname.charAt(0).toUpperCase()  + user.nickname.slice(1);
+      setTitle(`Hola ${capital}`);
+    }else{
+      setTitle("Hola")
+    }
+  }, [user, setTitle]);
   return (
     <StyledContainerActions isMobile={isMobile}>
       <Actions
